f27b0dc2c93a55e37b08422ab62f025c26c06e5d
barbahub <barbarianboy8@gmail.com>
2026-02-27 14:52:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GANTI ANGKA VERSI INI SETIAP KALI ANDA MENGUBAH INDEX.HTML
-const CACHE_NAME = 'amalpad-skena-v1.29'; 
+const CACHE_NAME = 'amalpad-skena-v1.30'; 
 
 const urlsToCache = [
     './',
@@ -48,6 +48,7 @@ self.addEventListener('fetch', event => {
         })
     );
 });
+
 
 
 
