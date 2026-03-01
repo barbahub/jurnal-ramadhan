864684fa552542f712906348f1f03a9b9782481c
barbahub <barbarianboy8@gmail.com>
2026-03-01 17:00:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GANTI ANGKA VERSI INI SETIAP KALI ANDA MENGUBAH INDEX.HTML
-const CACHE_NAME = 'amalpad-skena-v1.47'; 
+const CACHE_NAME = 'amalpad-skena-v1.48'; 
 
 const urlsToCache = [
   '/',
@@ -56,6 +56,7 @@ self.addEventListener('fetch', event => {
         })
     );
 });
+
 
 
 
